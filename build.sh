eb1b17beabba080bdc72e082f6a0ad98f8fa4d6c
Akhil143rjn <45694050+Akhil143rjn@users.noreply.github.com>
2020-02-12 22:20:09 +0530

    build script
    To build kernel faster.

---
+++ build.sh
@@ -0,0 +1,8 @@
+git clone --depth=1 https://github.com/DhirajMS/gcc-4.9-64 -b cm-14.1 tc
+export ARCH=arm64
+export CROSS_COMPILE=/home/akhil/kernel/tc/bin/aarch64-linux-android-
+export KBUILD_BUILD_USER="AkHIl"
+export KBUILD_BUILD_HOST="A3YN"
+mkdir -p out
+make O=out msmcortex_defconfig
+make O=out -j16
